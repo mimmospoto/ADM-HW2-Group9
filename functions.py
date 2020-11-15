52fba4b27f50cddced48eaccda09e082ca7defbd
Domenico <dspoto@hotmail.it>
2020-11-15 02:02:55 +0100

    Missing RQ3

--- functions.py
+++ functions.py
@@ -62,7 +62,363 @@ def make_new_csv(old_df_names, df_names):
         df.to_csv(df_names[i], index=False)
 
     return
-  
+
+# RQ1 functions
+
+# RQ1.1 functions
+def compute_average_session(df_names):
+    """
+    Compute average number of times users perform view/cart/purchase within each session
+    input:
+    - list of names of csv files to open
+    output:
+    - series of average of each operation
+    """    
+    # init the daily average dict
+    average_session_dict = {}
+
+    for i in range(len(df_names)):
+        average_session_dict[i] = {}
+        # load the ith dataframe, taking the event_type and user_session columns
+        df = pd.read_csv(df_names[i], usecols=['event_type', 'user_session'])
+
+        for j in df['event_type'].unique():
+            #print('{} of {:d} has average of : {:.2f} ' .format(j, i, float(df[df['event_type'] == j].groupby(['user_session']).count().mean())))
+            average_session_dict[i][j] = df[df['event_type'] == j].groupby(['user_session']).count().mean()
+
+    average_session_df = pd.DataFrame(average_session_dict).mean(axis=1)
+
+    return average_session_df
+
+def plot_average_session(average_session_df, months):
+    """
+    plots the average number of times users perform each operation
+    """
+    # plot average_session_df
+    fig = plt.figure()
+    X = np.arange(len(average_session_df))
+    plt.bar(X, average_session_df)
+    plt.xticks(np.arange(len(average_session_df)),average_session_df.index)
+    plt.ylabel("average operation per session")
+    plt.xlabel("operations")
+    plt.title("Average number of times users perform each operation within a session")
+    plt.grid(color ='silver', linestyle = ':')
+    fig.set_figwidth(15)
+    fig.set_figheight(5)
+
+    return
+
+# RQ1.2 functions
+def compute_average_view_cart(df_names, months):
+    """
+    Compute average number of times a user views a product before adding it to the cart
+    input:
+    - list of names of csv files to open
+    output:
+    - the average of how many times a product is viewed before to be added to the cart 
+    """   
+
+    # init a dataframe with index as every months and column as the mean for each user
+    df_mean_database = pd.DataFrame(index=months, columns=['mean'])
+    for i in range(len(df_names)):
+        # load the ith dataframe, taking the event_time, event_type, product_id, user_id columns
+        df = pd.read_csv(df_names[i],
+            usecols=['event_time','event_type', 'product_id', 'user_id'], nrows=100000,
+            parse_dates=['event_time'])
+
+        # cut off the 'purchase' variable from event_type
+        df_2 = df[df['event_type'] != 'purchase']
+        
+        df_3 = df_2[df_2.event_type=='view'].groupby(by=['product_id']).agg(view=('event_type', 'count'))
+
+        df_4 = df_2[df_2.event_type=='cart'].groupby(by=['product_id']).agg(cart=('event_type', 'count'))
+
+
+        # get dataframe where event_type is equal to 'cart'
+        df_cart = df_2[df_2['event_type']=='cart']
+
+        # init a dataframe with index as every user and column as the mean for each user
+        df_mean_user = pd.DataFrame(index=df_cart['user_id'].unique(), columns=['mean'])
+
+        df_cart.groupby(by=['user_id']).count()
+        for user in df_cart['user_id'].unique():
+            # get dataframe with one user at a time
+            df_user = df_2[df_2['user_id'] == user]
+            # init the dict where the key are the products and the values are the mean of each product
+            product_dict = {}
+
+            for prod in df_user['product_id'].unique():
+                # get dataframe with one product at a time
+                df_product = df_user[df_user['product_id'] == prod]
+                df_product_2 = df_product.copy()
+
+                product_dict[prod] = []
+
+                # init a list to append how many times 'view' appears before 'cart' for each product
+                product_lst = []
+
+                # check if at least a 'view' exist in the dataframe otherwise pass 
+                if any(df_product_2['event_type'] == 'view') == True:
+                    df_product_2_time = df_product_2[df_product_2['event_type'] == 'view'].event_time.reset_index(drop=True)[0]
+                
+                # check if there are some 'cart' event before the 'view' event (only for the first time of seeing the 'cart')
+                if any(df_product_2[df_product_2['event_type'] == 'cart'].event_time <= df_product_2_time) == True:
+                    df_product_3 = df_product_2[df_product_2.event_time <= df_product_2_time]
+                    # drop any 'cart' events at the beginning
+                    df_product_2 = df_product_2.drop(labels=df_product_3[df_product_3['event_type'] == 'cart'].index)
+
+                # count how many times 'view' is before 'cart'
+                if any(df_product_2['event_type'] == 'view') == True:
+                    for index, row in df_product_2.iterrows():
+                        
+                        if row['event_type'] == 'cart':
+                            
+                            product_lst.append(np.sum(df_product_2[df_product['event_type'] == 'view'].event_time < row['event_time']))
+                            df_product_2 = df_product_2[df_product_2.event_time > row['event_time']]
+                
+                # compute mean for each product
+                if len(product_lst) > 0:
+                    product_dict[prod] = [i for i in product_lst if i != 0]
+                    product_dict[prod] = np.mean(product_dict[prod])
+                else:
+                    product_dict[prod].append(0)
+            
+            # compute mean for each user
+            try:
+                df_mean_user.loc[user,'mean'] = round(pd.DataFrame(product_dict).mean(axis=1)[0], 2)
+            except ValueError:
+                df_mean_user.loc[user,'mean'] = round(product_dict[prod], 2)
+        
+        # compute final average for a user for a product
+        df_mean_user.dropna(inplace=True) 
+        mean_prod_user = np.mean(df_mean_user)
+    
+        # add final average per month
+        df_mean_database.loc[months[i], 'mean'] =  round(mean_prod_user[0], 2)
+
+    df_mean_database.dropna(inplace=True) 
+    final_mean = np.mean(df_mean_database)    
+    return final_mean
+
+# RQ1.3 functions
+def compute_probability_cart_purchase(df_names, months):
+    """
+    Compute the probability that products are bought once is added to the cart
+    input:
+    - list of names of csv files to open
+    output:
+    - probability products are purchased once are added to the cart
+    """   
+    # init dictionary to merge each monthly datasets 
+    df_database = {}
+    for i in range(len(df_names)):
+        # load the ith dataframe, taking only the event_type
+        df = pd.read_csv(df_names[i],
+            usecols=['event_type'], na_filter=False)
+        
+        # cut off the view variable from event_type
+        df_database[months[i]] = df[df['event_type'] != 'view']
+
+    # function to concatenate each dataset
+    merged_df = pd.concat([df_database[months[i]] for i in range(len(df_database))])
+    # compute probability as the ratio between purchase and cart events
+    prob = round(merged_df[merged_df['event_type'] == 'purchase'].shape[0] / 
+                 merged_df[merged_df['event_type'] == 'cart'].shape[0], 4) * 100
+    
+    return prob
+
+# RQ1.4 functions
+def compute_average_time_removed_item(df_names):
+    """
+    Compute the average time an item stays in the cart before being removed
+    input:
+    - list of names of csv files to open
+    output:
+    - average time
+    """      
+    df_mean_database = pd.DataFrame(index=months, columns=['mean'])
+    for i in range(len(df_names)):
+        # load the ith dataframe, taking only the 
+        df = pd.read_csv(df_names[i],
+            usecols=['event_time', 'event_type', 'product_id'], nrows=100000,
+            parse_dates=['event_time'])
+
+        # cut off the view variable from event_type
+        df_2 = df[df['event_type'] != 'view']
+        
+        # init the dict where the key are the products and the values are the mean of each product
+        product_dict = {}
+        # loop through the event_type 'purchase' to find unique product_id
+        for prod in df_2[df_2['event_type'] == 'purchase']['product_id'].unique():
+            df_product = df_2[df_2['product_id'] == prod]
+
+            # check if at least a 'cart' event exist             
+            if df_product['event_type'].str.contains('cart').any():
+                pass
+            else:
+                continue
+
+            # check if there are some 'purchase' event before the 'cart' event (only for the first time of seeing the 'purchase')
+            if any(df_product[df_product['event_type'] == 'purchase'].event_time <= 
+                    df_product[df_product['event_type'] == 'cart'].event_time.reset_index(drop=True)[0]) == True:
+                df_3 = df_product[df_product.event_time <= df_product[df_product['event_type'] == 'cart'].event_time.reset_index(drop=True)[0]]
+                # drop any 'cart' events at the beginning
+                df_product = df_product.drop(labels=df_3[df_3['event_type'] == 'purchase'].index)
+
+            
+            dist_prod = df_product.event_time[df_product.event_type == 'purchase'].values - df_product.event_time[df_product.event_type == 'cart'].values
+
+            product_dict[prod].append(np.mean(dist_prod))
+
+        # add final average per month
+        df_mean_database.loc[months[i], 'mean'] =  pd.DataFrame(product_dict).mean(axis=1)[0]
+
+# RQ1.5 functions
+def compute_average_time_first_view(df_names, months):
+    """
+    Compute the probability that products are bought once is added to the cart
+    input:
+    - list of names of csv files to open
+    output:
+    - probability products are purchased once are added to the cart
+    """   
+    
+    df_mean_database = pd.DataFrame(index=months, columns=['mean'])
+    for i in range(len(df_names)):
+        # load the ith dataframe, taking only the 
+        df = pd.read_csv(df_names[i],
+            usecols=['event_time', 'event_type', 'product_id'],
+            parse_dates=['event_time'])
+
+        # cut off the view variable from event_type
+        df_2 = df.copy()
+        
+        df_3 = df[df['event_type'] != 'view']
+
+        # init the dict where the key are the products and the values are the mean of each product
+        product_dict = {}
+        # loop through the event_type 'purchase' to find unique product_id
+        for prod in df_3['product_id'].unique():
+            df_product = df_2[df_2['product_id'] == prod]
+            
+            # 
+            if any(df_product['event_type'] == 'view') == False:
+                continue
+
+            # check if there are some 'purchase' event before the 'view' event (only for the first time of seeing the 'purchase')
+            if any(df_product[df_product['event_type'] == 'purchase'].event_time <= 
+                df_product[df_product['event_type'] == 'view'].event_time.reset_index(drop=True)[0]) == True:
+                df_3 = df_product[df_product.event_time <= df_product[df_product['event_type'] == 'view'].event_time.reset_index(drop=True)[0]]
+                # drop any 'cart' events at the beginning
+                df_product = df_product.drop(labels=df_3[df_3['event_type'] == 'purchase'].index)
+        
+            # check if there are some 'cart' event before the 'view' event (only for the first time of seeing the 'purchase')
+            if any(df_product[df_product['event_type'] == 'cart'].event_time <= 
+                    df_product[df_product['event_type'] == 'view'].event_time.reset_index(drop=True)[0]) == True:
+                df_3 = df_product[df_product.event_time <= df_product[df_product['event_type'] == 'view'].event_time.reset_index(drop=True)[0]]
+                # drop any 'cart' events at the beginning
+                df_product = df_product.drop(labels=df_3[df_3['event_type'] == 'cart'].index)
+
+            # 
+            if any(df_product['event_type'] == 'purchase') == False:
+                continue
+            elif any(df_product['event_type'] == 'cart') == False:
+                continue
+
+            product_dict[prod] = []
+
+            df_product.drop_duplicates(subset=['event_type'], keep='first', inplace=True)
+
+            df_product.reset_index(inplace=True) 
+            product_dict[prod].append(df_product.event_time[1] - df_product.event_time[0])
+        
+    
+        # add final average per month
+        df_mean_database.loc[months[i], 'mean'] =  pd.DataFrame(product_dict).mean(axis=1)[0]
+        
+    return df_mean_database
+        
+
+# RQ2 functions
+
+def compute_number_sold_per_category(df_names, months):
+    """
+    Compute the probability that products are bought once is added to the cart
+    input:
+    - list of names of csv files to open
+    output:
+    - 
+    """    
+    # init a dataframe with index as months and column as most sold product
+    df_final = pd.DataFrame(columns=months)
+    df_final_2 = {}
+    for i in range(len(df_names)):
+        # load the ith dataframe, taking only the 
+        df = pd.read_csv(df_names[i],
+            usecols=['product_id', 'category_code'])
+
+        new = df['category_code'].str.split(".", expand=True)
+        df['category_1'] = new[0]
+        df['category_1'].unique()
+        df.drop(columns=['category_code'], inplace=True)
+
+        df_final[months[i]] = pd.DataFrame(df.groupby(by=['category_1']).count().sort_values('product_id', ascending=False), columns=months[i])
+        df_final_2[months[i]] = pd.DataFrame(df.groupby(by=['category_1']).count().sort_values('product_id', ascending=False))
+
+    pd.DataFrame(df_final_2)
+
+
+# RQ4 functions
+def make_df_purchase(df_names, months):
+    df_purchase = {}
+    for i in range(len(df_names)):
+        data = pd.read_csv(df_names[i], usecols=['brand', 'price', 'event_type'], nrows=1000000)
+        df_purchase[months[i]] = data[data['event_type'] == 'purchase']
+    return df_purchase
+
+def earning_per_month(df_purchase, months):
+    dict_earning = {}
+    for i in range(len(df_purchase)):
+        data = df_purchase[months[i]]
+        dict_earning[months[i]] = data.loc[data.event_type == 'purchase'].groupby('brand', as_index=False).sum()
+    return dict_earning
+
+def brand_per_month(brand, dict_earning, months):
+    df_profit = {}
+    for i in range(len(months)):
+        try:
+            df_profit[months[i]] = dict_earning[months[i]].loc[dict_earning[months[i]].brand == brand, 'price'].values[0]
+        except IndexError:
+            df_profit[months[i]] = 0
+    return df_profit
+
+def find_3_worst_brand(dict_earning, months):
+    data_frames = [dict_earning[months[i]] for i in range(len(dict_earning))]
+
+    df_merged = reduce(lambda  left,right: pd.merge(left,right,on=['brand'],
+                                        how='outer'), data_frames)
+
+    df_merged.set_index('brand', inplace=True)
+    df_merged.set_axis(months, axis=1, inplace=True)
+
+    df_pct = df_merged.T.pct_change()
+
+    worst_brand = []
+    worst_value = []
+    worst_months = []
+
+    for i in range(0,3):
+
+        worst_brand.append(df_pct.min().sort_values().index[i])
+        worst_value.append(round(abs(df_pct.min().sort_values()[i])*100, 2))
+
+        L = list(df_pct[df_pct[worst_brand[i]] == df_pct.min().sort_values()[i]].index.values)
+        worst_months.append(''.join(L))
+
+
+    for j in range(0,3):
+        print('{} lost {}% bewteen {} and the month before'.format(worst_brand[j], worst_value[j], worst_months[j]), end=' \n')
+    return   
 
 #RQ5
 #Function that create a plot that for each day of the week shows the hourly average of visitors
